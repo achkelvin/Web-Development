d0a8930f5d246f37bab54e0f921cd9cd4113b31b
achkelvin <a.kelvintri@gmail.com>
2024-01-15 23:07:18 +0700

    100 Control Statements Using If Else to Love Calculator (answered)

--- Section 16 Intermediate to Javascript/loveCalculator.js
+++ Section 16 Intermediate to Javascript/loveCalculator.js
@@ -3,6 +3,21 @@ function loveCalculator() {
   var yourName = prompt('What is their name?');
   var loveScore = Math.random() * 100;
   loveScore = Math.floor(loveScore) + 1;
+  if (loveScore > 70) {
+    alert(
+      myName +
+        ' and ' +
+        yourName +
+        ' have a love score of ' +
+        loveScore +
+        '%. You love each other like Kanye loves Kanye.'
+    );
+  }
+  if (loveScore > 30 && loveScore <= 70) {
+    alert(
+      myName + ' and ' + yourName + ' have a love score of ' + loveScore + '%'
+    );
+  }
   return alert(
     myName + ' and ' + yourName + ' have a love score of ' + loveScore + '%'
   );
