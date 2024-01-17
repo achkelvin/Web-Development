d9384b741556e2e3435ec497f50935bfb6d22451
achkelvin <a.kelvintri@gmail.com>
2024-01-17 15:30:55 +0700

    104. Leap Year Challange (added alternative code)

--- Section 15 Intermediate to Javascript/leapYear.js
+++ Section 15 Intermediate to Javascript/leapYear.js
@@ -65,3 +65,21 @@ function isLeap(year) {
 }
 
 console.log(isLeap(2400));
+
+// or;
+
+function isLeap(year) {
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) {
+        return 'Leap year.';
+      } else {
+        return 'Not leap year.';
+      }
+    } else {
+      return 'Leap year.';
+    }
+  } else {
+    return 'Not leap year.';
+  }
+}
