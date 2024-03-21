0eb7b65e9d0a014b2cd81293935aaeb98e24c476
achkelvin <a.kelvintri@gmail.com>
2024-03-21 23:11:01 +0700

    116. Complete DOM Challange

---
+++ Section 16 The Document Object Model(DOM)/DOM Challenge Starting Files/index.js
@@ -0,0 +1,6 @@
+document.querySelector('ul').lastElementChild.innerHTML = 'Kelvin';
+
+// Or you can use the following code:
+// document.firstElementChild.lastElementChild.firstElementChild.lastElementChild.innerHTML = 'Kelvin';
+// and
+// document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = 'Kelvin';
