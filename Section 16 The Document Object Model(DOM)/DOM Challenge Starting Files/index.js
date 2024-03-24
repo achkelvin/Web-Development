24fe8f2bdb68e710100e3bc5270febda70805548
achkelvin <a.kelvintri@gmail.com>
2024-03-24 23:21:07 +0700

    119. Seperation: Structure vs Style vs Behaviour (noted and answered)

--- Section 16 The Document Object Model(DOM)/DOM Challenge Starting Files/index.js
+++ Section 16 The Document Object Model(DOM)/DOM Challenge Starting Files/index.js
@@ -6,3 +6,4 @@ document.querySelector('ul').lastElementChild.innerHTML = 'Kelvin';
 // document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = 'Kelvin';
 document.querySelector('ul li a').style.color = 'red';
 document.querySelector('button').style.backgroundColor = 'yellow';
+document.querySelector('h1').classList.add('huge');
